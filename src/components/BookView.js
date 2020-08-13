4be29d8ee008614488711f475574d7f55ad700cb
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2020-08-13 07:26:41 +0200

    fix: Display cover images properly

--- src/components/BookView.js
+++ src/components/BookView.js
@@ -3,23 +3,8 @@ import PropTypes from 'prop-types';
 
 const BookView = (props) => (
   <div className="book">
-    <div className="book-top">
-      <div className="book-cover"
-        style={{
-          width: 128,
-          height: 193,
-          backgroundImage: `url("${props.coverImage}")`
-        }}>
-      </div>
-      {/*<div className="book-shelf-changer">
-        <select>
-          <option value="move" disabled>Move to...</option>
-          <option value="currentlyReading">Currently Reading</option>
-          <option value="wantToRead">Want to Read</option>
-          <option value="read">Read</option>
-          <option value="none">None</option>
-        </select>
-      </div>*/}
+    <div className="book-cover">
+      <img src={props.coverImage} />
     </div>
     <div className="book-title">{props.title}</div>
     <div className="book-authors">
