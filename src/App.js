1caa9ea6a231ef2b6fdbf21c75038986e6f3580f
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2020-08-13 08:25:32 +0200

    feat: Add search capability

--- src/App.js
+++ src/App.js
@@ -33,7 +33,18 @@ class BooksApp extends React.Component {
   ]
   constructor(props) {
     super(props);
-    BooksAPI.search('Web Development')
+    this.state = {
+      searchValue: ''
+    }
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+  }
+
+  handleSearchChange(e) {
+    const value = e.target.value;
+    this.setState({
+      searchValue: value
+    });
+    BooksAPI.search(value)
       .then(result => console.log(result));
   }
 
@@ -45,7 +56,10 @@ class BooksApp extends React.Component {
             <BookShelves />
           </Route>
           <Route path="/search">
-            <BookSearch />
+            <BookSearch
+              searchValue={this.state.searchValue}
+              handleSearchChange={this.handleSearchChange}
+            />
           </Route>
         </Switch>
       </BrowserRouter>
@@ -56,26 +70,7 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <div />
         ) : (
           <div className="list-books">
             <div className="list-books-title">
--- src/components/BookSearch.js
+++ src/components/BookSearch.js
@@ -3,11 +3,28 @@ import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 
 const BookSearch = (props) => (
-  <Link to="/">Shelves</Link>
+  // TODO: use bookshelf component to display results
+  <div className="search-books">
+    <div className="search-books-bar">
+      <Link to="/" className="close-search">Close</Link>
+      <div className="search-books-input-wrapper">
+        <input
+          type="text"
+          placeholder="Search by title or author"
+          value={props.searchValue}
+          onChange={props.handleSearchChange}
+        />
+      </div>
+    </div>
+    <div className="search-books-results">
+      <ol className="books-grid"></ol>
+    </div>
+  </div>
 );
 
 BookSearch.propTypes = {
-
+  searchValue: PropTypes.string.isRequired,
+  handleSearchChange: PropTypes.func.isRequired
 };
 
 export default BookSearch;
