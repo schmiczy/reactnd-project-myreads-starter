48e0e6aa852327b3b39bb3d5be880d7d8553f6f1
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2020-08-20 19:15:47 +0200

    fix: Add alt text to cover images

--- src/components/BookView.js
+++ src/components/BookView.js
@@ -4,7 +4,10 @@ import PropTypes from 'prop-types';
 const BookView = (props) => (
   <div className="book">
     <div className="book-cover">
-      <img src={props.coverImage} />
+      <img
+        src={props.coverImage}
+        alt={`Book cover of ${props.title}${props.authors.length > 0 ? ' from ' + props.authors[0] : ''}${props.authors.length > 1 ? ' and others' : ''}`}
+      />
     </div>
     <div className="book-title">{props.title}</div>
     <div className="book-authors">
